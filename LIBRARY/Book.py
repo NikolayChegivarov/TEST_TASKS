b46bc6921c5408e310f8c6c4db3dcc8e7ea0b7de
Nikolai Gusarov <nikolai_polos@mail.ru>
2024-11-27 13:43:21 +0300

    comment

--- LIBRARY/Book.py
+++ LIBRARY/Book.py
@@ -8,15 +8,18 @@ class Book:
         self.status = "в наличии"
 
     def change_status(self, new_status: str) -> None:
+        """Изменяет статус книги. Проверяет на валидность."""
         if new_status.lower() in ["в наличии", "выдана"]:
             self.status = new_status
         else:
             raise ValueError("Неверный статус")
 
     def __str__(self):
+        """Возвращает строковое представление объекта Book с информацией о книге."""
         return f"ID: {self.id}\nTitle: {self.title}\nAuthor: {self.author}\nYear: {self.year}\nStatus: {self.status}"
 
     def to_dict(self):
+        """Преобразует объект Book в словарь со всеми полями."""
         return {
             "id": self.id,
             "title": self.title,
